78ec4a8ef44b594a61c4bfbbb6808dd9320da1d4
sqldan <danrrobertson@gmail.com>
2017-05-07 23:07:35 +0100

    Added missing file

---
+++ PlayingWithTempDB_ViaSMO.ps1
@@ -0,0 +1,25 @@
+﻿#import-module sqlps
+#c:\
+
+$server = new-object Microsoft.SqlServer.Management.Smo.Server(".")
+$tempdb = $server.Databases.Item("tempdb")
+
+$primaryGroup = $tempdb.FileGroups.item("primary")
+$firstFile = $primaryGroup.files.Item("tempdev")
+$secondFile = $primaryGroup.files.Item("tempdb2")
+
+#$secondFile.drop()
+
+$DataFileObject = New-Object Microsoft.SqlServer.Management.Smo.DataFile
+
+$DataFileObject.Parent = $primaryGroup
+$DataFileObject.Name = "tempdb2"
+$DataFileObject.FileName = join-path $server.DefaultFile "tempdb2.mdf"
+$DataFileObject.Size = 8192
+$DataFileObject.GrowthType = "None"
+
+$DataFileObject.Create()
+
+$server.DefaultFile
+
+
